7dd4165e95e01e50504242aed00d0d4c3d52fc2d
gauravnetes <gouravchandra935@gmail.com>
2025-10-23 01:44:11 +0530

    Cover In Water

---
+++ 800/Cover_In_Water.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        bool continuous_3_empty_cells = false;
+        int total_count_empty_cells = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '.' && i + 1 < n && s[i + 1] == '.' && i + 2 < n && s[i + 2] == '.')
+            {
+                continuous_3_empty_cells = true; 
+                break; 
+            }
+
+            if(s[i] == '.')
+                total_count_empty_cells++; 
+        }
+
+        if(continuous_3_empty_cells) 
+            cout << 2 << endl; 
+        else
+            cout << total_count_empty_cells << endl; 
+    }
+    return 0; 
+}
